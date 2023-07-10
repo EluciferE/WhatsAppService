f836561179ac144b3bb731d3c5741cf7495c1aba
Meow <luciferevgeniy@bk.ru>
2023-07-10 15:05:56 +0600

    [UPDATE] Force elements to use SearchElement class

---
+++ whats_app_browser/elements/common_elements.py
@@ -0,0 +1,4 @@
+from selenium.webdriver.common.by import By
+from ..types import SearchElement
+
+IMAGE = SearchElement(By.TAG_NAME, "img")
--- whats_app_browser/elements/elements_xpath.py
+++ whats_app_browser/elements/elements_xpath.py
@@ -1,6 +1,5 @@
 class ELEMENT_XPATH:
     PROFILE_SIDEBAR = "/html/body/div[1]/div/div/div[6]/span/div/span/div/div/section"
-    PROFILE_BIG_PIC = "/html/body/div[1]/div/div/div[6]/span/div/span/div/div/section/div[1]/div[2]/div/div"
     PROFILE_SMALL_PIC = "/html/body/div[1]/div/div/div[5]/div/header/div[1]/div"
 
     CHAT_POPUP = "/html/body/div[1]/div/span[2]/div/span/div/div/div/div/div/div[1]"
