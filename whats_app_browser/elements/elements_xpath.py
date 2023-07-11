f4c239c1ba4a4948b3f89089f3bbc1a66371ef5f
Meow <luciferevgeniy@bk.ru>
2023-07-11 09:42:26 +0600

    [ADD] Add new elements to check

--- whats_app_browser/elements/elements_xpath.py
+++ whats_app_browser/elements/elements_xpath.py
@@ -3,3 +3,6 @@ class ELEMENT_XPATH:
     PROFILE_SMALL_PIC = "/html/body/div[1]/div/div/div[5]/div/header/div[1]/div"
 
     CHAT_POPUP = "/html/body/div[1]/div/span[2]/div/span/div/div/div/div/div/div[1]"
+    LOGOUT_BUTTON = "/html/body/div[1]/div/div/div[5]/div/div/div"
+
+    QR_PRELOAD = "/html/body/div[1]/div/div/div[3]/div[1]/div/div/div[2]/div/span/svg"
--- whats_app_browser/elements/whatsapp_elements.py
+++ whats_app_browser/elements/whatsapp_elements.py
@@ -4,9 +4,13 @@ from .elements_xpath import ELEMENT_XPATH
 from selenium.webdriver.common.by import By
 
 QR_CODE = SearchElement(By.TAG_NAME, "canvas")
+QR_PRELOAD = SearchElement(By.XPATH, ELEMENT_XPATH.QR_PRELOAD)
+
 CHATS_SIDEBAR = SearchElement(By.ID, "side")
 CURRENT_CHAT = SearchElement(By.ID, "main")
 POPUP = SearchElement(By.XPATH, ELEMENT_XPATH.CHAT_POPUP)
 
 PROFILE_SMALL_PIC = SearchElement(By.XPATH, ELEMENT_XPATH.PROFILE_SMALL_PIC)
 PROFILE_SIDEBAR = SearchElement(By.XPATH, ELEMENT_XPATH.PROFILE_SIDEBAR)
+
+LOGOUT_BUTTON = SearchElement(By.XPATH, ELEMENT_XPATH.LOGOUT_BUTTON)
